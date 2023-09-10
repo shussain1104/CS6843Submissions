a2b8076b8651a2480c2ead6734f565cabb6f8bea
Shahreen <shussain1104@gmail.com>
2023-09-10 16:47:04 -0400

    V5

--- gettingStarted.py
+++ gettingStarted.py
@@ -1,6 +1,8 @@
 ### welcome_assignment_answers
 ### Input - All nine questions given in the assignment.
 ### Output - The right answer for the specific question.
+from hashlib import sha256
+
 
 def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
@@ -10,13 +12,13 @@ def welcome_assignment_answers(question):
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "no"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
     elif question == "Is a hashed message supposed to be unhashed? - Yes/No":
         answer = 'No'
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-        answer = "e92581c4d3acd68a5b9987b4f17522ca75a63b736f5d5f1a9efcaa3ed2c01cca"
+        answer = sha256("sh7859@nyu.edu")
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No ":
         answer = 'No'
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
