6e0336637ce289e5feb2696449f6af7721e697f1
Shahreen <shussain1104@gmail.com>
2023-09-10 16:53:54 -0400

    V6

--- gettingStarted.py
+++ gettingStarted.py
@@ -15,11 +15,11 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
-    elif question == "Is a hashed message supposed to be unhashed? - Yes/No":
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
         answer = 'No'
     elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-        answer = sha256("sh7859@nyu.edu")
-    elif question == "Is MD5 a secured hashing algorithm? - Yes/No ":
+        answer = 'e92581c4d3acd68a5b9987b4f17522ca75a63b736f5d5f1a9efcaa3ed2c01cca'
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = 'No'
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
         answer = 5
