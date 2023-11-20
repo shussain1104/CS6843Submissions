fd38ce763482b9ea21bd95c329b4bf4ed363e538
Shahreen <shussain1104@gmail.com>
2023-11-20 18:16:43 -0500

    Updated Values

--- dnsClient.py
+++ dnsClient.py
@@ -142,9 +142,9 @@ def dns_query(type, name, server):
         name = '.'.join(name_parts)
 
         # Parse the type, class, TTL, and RDLENGTH
-        type, cls, ttl, rdlength = struct.unpack('!HHIH', response_answer[offset:offset+16+2+4+2]) # What is the offset value in bytes? Remember 'H' represent 2 bytes, and 'I' represents one byte, we declared '!HHIH'.
+        type, cls, ttl, rdlength = struct.unpack('!HHIH', response_answer[offset:offset+2+4+2]) # What is the offset value in bytes? Remember 'H' represent 2 bytes, and 'I' represents one byte, we declared '!HHIH'.
         
-        offset += 16 + 2 + 4 + 2# Same value as just calculated
+        offset += 2 + 4 + 2# Same value as just calculated
 
         # Parse the RDATA
         rdata = response_answer[offset:offset+rdlength]
